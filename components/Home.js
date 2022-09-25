560d3a6a1dc0a877bf47b705e1d7b6f93b02ccb4
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2022-09-25 16:06:05 -0500

    Homepage done, responsive, no animation

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,83 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import {IoIosArrowDown} from 'react-icons/io'
 
 const Home = ({myFunction}) => {
+  
+  const parent = {
+
+    hidden: {
+      opacity: 0
+    },
+
+    show: {
+      opacity: 1,
+      transition:{staggerChildren:.1}
+    }
+  }
+
+  const child = {
+
+    hidden:{
+      opacity: 0
+    },
+
+    show:{
+      opacity: 1,
+    }
+  }
+
+  const parent2 = {
+
+    hidden: {
+      opacity: 0
+    },
+
+    show: {
+      opacity: 1,
+      transition:{staggerChildren:.2, delay: 1}
+    }
+  }
+
+  const child2 = {
+
+    hidden:{
+      opacity: 0
+    },
+
+    show:{
+      opacity: 1,
+    }
+  }
+
+  const myName = 'Nikhil Dronamraju'
+  const myNameAsArray = myName.split("")
+  //console.log(myNameAsArray)
+  const myNameAutoFill = myNameAsArray.map(
+    (letter, index) => {
+      //console.log(`myName ${index}`)
+      return (
+        <motion.span key={`myName ${index}`} variants={child}>{letter}</motion.span>
+      )
+    }
+  )
+  const myTitle = 'Full Stack Developer. Designer. Creator.'
+  const myTitleAsArray = myTitle.split("")
+  //console.log(myNameAsArray)
+  const myTitleAutoFill = myTitleAsArray.map(
+    (letter, index) => {
+      //console.log(`myName ${index}`)
+      return (
+        <motion.span key={`myTitle ${index}`} variants={child2}>{letter}</motion.span>
+      )
+    }
+  )
   return (
-    <div className='border flex flex-col'>
-        <motion.h1 className='text-5xl text-center'>Nikhil Dronamraju.</motion.h1>
-        <motion.h3 className='border ml-20 text-center'>Full Stack Developer. Web Designer. Creator.</motion.h3>
+    <div className='border flex flex-col h-screen bg-zinc-400'>
+        <motion.h1 className='text-4xl md:text-6xl lg:text-8xl text-center pt-36 text-slate-50' variants={parent} initial='hidden' animate={'show'}>{myNameAutoFill}</motion.h1>
+        <motion.h1 className='text-1xl md:text-2xl lg:text-3xl text-center text-sky-300' variants={parent2} initial='hidden' animate={'show'}>{myTitleAutoFill}</motion.h1>
         <motion.button onClick={() => myFunction()}>
-              Click me to move down
+              <IoIosArrowDown color='white' className='ml-auto mr-auto mt-32 text-xl md:text-2xl lg:text-4xl'/>
         </motion.button>
     </div>
   )
